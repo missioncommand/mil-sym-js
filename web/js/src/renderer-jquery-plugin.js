64170d23f6422902bc13bcd676b5e12b7f2201eb
Michael Spinelli <michael.spinelli@us.army.mil>
2014-03-06 09:08:47 -0500

    -added keep-unit-ratio & sanitize properties -sanitize will strip all extras off a symbol (hq staff, echelon, mobility, affiliation modifiers, etc...) so that just the core symbol is rendered.  Ideal for use in tree nodes or small icons not on a map.

--- web/js/src/renderer-jquery-plugin.js
+++ web/js/src/renderer-jquery-plugin.js
@@ -2,6 +2,45 @@
     $.fn.render2525 = function() {
         var msa = armyc2.c2sd.renderer.utilities.MilStdAttributes;
         
+        /**
+         * we only have font lookups for F,H,N,U.  But the shapes match one of these
+         * four for the remaining affiliations.  So we convert the string to a base
+         * affiliation before we do the lookup.
+         * @param {String} symbolID
+         * @returns {String}
+         */        
+        function sanitize(symbolID) {
+            var code = symbolID.substring(0);
+            var affiliation = symbolID.charAt(1);
+
+            if(affiliation === "F" ||//friendly
+                    affiliation === "H" ||//hostile
+                    affiliation === "U" ||//unknown
+                    affiliation === "N" )//neutral
+                return code;
+            else if(affiliation === "S")//suspect
+                code = code.charAt(0) + "H" + code.substring(2, 15);
+            else if(affiliation === "L")//exercise neutral
+                code = code.charAt(0) + "N" + code.substring(2, 15);
+            else if(affiliation === "A" ||//assumed friend
+                    affiliation === "D" ||//exercise friend
+                    affiliation === "M" ||//exercise assumed friend
+                    affiliation === "K" ||//faker
+                    affiliation === "J")//joker
+                code = code.charAt(0) + "F" + code.substring(2, 15);
+            else if(affiliation === "P" ||//pending
+                    affiliation === "G" ||//exercise pending
+                    affiliation === "O" ||//? brought it over from mitch's code
+                    affiliation === "W")//exercise unknown
+                code = code.charAt(0) + "U" + code.substring(2, 15);
+            else
+                code = code.charAt(0) + "U" + code.substring(2, 15);
+
+            code = code.substring(0,10) + "-----";
+
+            return code;
+        };
+        
         return this.filter("canvas").each(function() {
             // The only required data attribute is the symbol code, only
             // continue if it's there
@@ -18,11 +57,27 @@
                 else {
                     modifiers[msa.PixelSize] = $(this).data("pixel-size");
                 }
+                
+                //if false, will fill the space. if true, will size with respect to other symbols.
+                if ($(this).data("keep-unit-ratio") === undefined) {
+                    // Use 32 as the default size
+                    modifiers[msa.KeepUnitRatio] = true;
+                }
+                else {
+                    modifiers[msa.KeepUnitRatio] = $(this).data("keep-unit-ratio");
+                }
 
-                // Default to keeping unit ratios
-                modifiers[msa.KeepUnitRatio] = true;
+                var symbolID = $(this).data("symbol-code");
+                
 
-                ii = armyc2.c2sd.renderer.MilStdIconRenderer.Render($(this).data("symbol-code"), modifiers);
+                if ($(this).data("sanitize") === true) {   
+                    if(symbolID.charAt(1) === 'J' || symbolID.charAt(1) === 'K'){
+                        modifiers[msa.FillColor] = "#FF0000";
+                    }
+                    symbolID = sanitize(symbolID, modifiers);   
+                }
+
+                ii = armyc2.c2sd.renderer.MilStdIconRenderer.Render(symbolID, modifiers);
                 if (ii !== undefined)
                 {
                     var canvasObject = $(this).get(0);
