12063cd16df36d2cced9541948a0166c0c6404f4
Michael Deutch <mdeutch3@comcast.net>
2014-04-21 17:22:32 -0400

    Fixed error in Airfield when fill is set.

--- web/js/src/armyc2/c2sd/JavaTacticalRenderer/clsUtility.js
+++ web/js/src/armyc2/c2sd/JavaTacticalRenderer/clsUtility.js
@@ -898,6 +898,10 @@ armyc2.c2sd.JavaTacticalRenderer.clsUtility = {
                     }
                 }
                 armyc2.c2sd.JavaTacticalRenderer.clsUtility.ResolveModifierShape(tg, shape);
+                if(lineType===221311000)    //AIRFIELD
+                    if(j==1)
+                        shape.setFillColor(null);
+                
                 shapeType = shape.getShapeType();
                 if (lineType === 22123000) {
                     armyc2.c2sd.JavaTacticalRenderer.clsUtility.SetLCColor(tg, shape);
