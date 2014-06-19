474ddf54b8a94dcb80988df27e19979911ca2f68
Michael Spinelli <michael.spinelli@us.army.mil>
2014-06-19 16:30:15 -0400

    changes for functions supporting getSinglePointInfo.

--- web/js/src/sec/web/renderer/utilities/JavaRendererUtilities.js
+++ web/js/src/sec/web/renderer/utilities/JavaRendererUtilities.js
@@ -6,8 +6,6 @@ sec.web.renderer.utilities = sec.web.renderer.utilities || {};
 /** @class */
 sec.web.renderer.utilities.JavaRendererUtilities = {};
 
-    sec.web.renderer.utilities.JavaRendererUtilities.be = "";
-    
     sec.web.renderer.utilities.JavaRendererUtilities.getBrowserEngine = function()
     {
         var ua = navigator.userAgent;
@@ -19,175 +17,6 @@ sec.web.renderer.utilities.JavaRendererUtilities = {};
             return "Gecko";//Firefox
         else
             return "Other";
-    };
-    
-    sec.web.renderer.utilities.JavaRendererUtilities.getBrowserInfo = function()
-    {
-         //rendering engines
-         var engine = {
-             ie: 0,
-             gecko: 0,
-             webkit: 0,
-             khtml: 0,
-             opera: 0,
-
-             //complete version
-             ver: null
-         };
-
-         //browsers
-         var browser = {
-
-             //browsers
-             ie: 0,
-             firefox: 0,
-             safari: 0,
-             konq: 0,
-             opera: 0,
-             chrome: 0,
-             safari: 0,
-
-             //specific version
-             ver: null
-         };
-
-
-         //platform/device/OS
-         var system = {
-             win: false,
-             mac: false,
-             x11: false,
-
-             //mobile devices
-             iphone: false,
-             ipod: false,
-             nokiaN: false,
-             winMobile: false,
-             macMobile: false,
-
-             //game systems
-             wii: false,
-             ps: false
-         };
-
-         //detect rendering engines/browsers
-         var ua = navigator.userAgent;
-         if (window.opera){
-             engine.ver = browser.ver = window.opera.version();
-             engine.opera = browser.opera = parseFloat(engine.ver);
-         } else if (/AppleWebKit\/(\S+)/.test(ua)){
-             engine.ver = RegExp["$1"];
-             engine.webkit = parseFloat(engine.ver);
-
-             //figure out if it's Chrome or Safari
-             if (/Chrome\/(\S+)/.test(ua)){
-                 browser.ver = RegExp["$1"];
-                 browser.chrome = parseFloat(browser.ver);
-             } else if (/Version\/(\S+)/.test(ua)){
-                 browser.ver = RegExp["$1"];
-                 browser.safari = parseFloat(browser.ver);
-             } else {
-                 //approximate version
-                 var safariVersion = 1;
-                 if (engine.webkit < 100){
-                     safariVersion = 1;
-                 } else if (engine.webkit < 312){
-                     safariVersion = 1.2;
-                 } else if (engine.webkit < 412){
-                     safariVersion = 1.3;
-                 } else {
-                     safariVersion = 2;
-                 }
-
-                 browser.safari = browser.ver = safariVersion;
-             }
-         } else if (/KHTML\/(\S+)/.test(ua) || /Konqueror\/([^;]+)/.test(ua)){
-             engine.ver = browser.ver = RegExp["$1"];
-             engine.khtml = browser.konq = parseFloat(engine.ver);
-         } else if (/rv:([^\)]+)\) Gecko\/\d{8}/.test(ua)){
-             engine.ver = RegExp["$1"];
-             engine.gecko = parseFloat(engine.ver);
-
-            //determine if it's Firefox
-            if (/Firefox\/(\S+)/.test(ua))
-            {
-                browser.ver = RegExp["$1"];
-                browser.firefox = parseFloat(browser.ver);
-            }
-         } else if (/MSIE ([^;]+)/.test(ua)){
-             engine.ver = browser.ver = RegExp["$1"];
-             engine.ie = browser.ie = parseFloat(engine.ver);
-
-         }
-         else if (/Trident\/(\S+)/.test(ua)){
-             engine.ver = browser.ver = RegExp["$1"];
-             engine.ie = browser.ie = parseFloat(engine.ver) + 4;
-             //5=IE9, 6=IE10, 7=IE11
-         }
-
-         //detect browsers
-         browser.ie = engine.ie;
-         browser.opera = engine.opera;
-
-
-         //detect platform
-         var p = navigator.platform;
-         system.win = p.indexOf("Win") == 0;
-         system.mac = p.indexOf("Mac") == 0;
-         system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-
-         //detect windows operating systems
-         if (system.win){
-             if (/Win(?:dows )?([^do]{2})\s?(\d+\.\d+)?/.test(ua)){
-                 if (RegExp["$1"] == "NT"){
-                     switch(RegExp["$2"]){
-                         case "5.0":
-                             system.win = "2000";
-                             break;
-                         case "5.1":
-                             system.win = "XP";
-                             break;
-                         case "6.0":
-                             system.win = "Vista";
-                             break;
-                         case "6.1":
-                             system.win = "7";
-                             break;
-                         case "6.2":
-                             system.win = "8";
-                             break;
-                         case "6.3":
-                             system.win = "8.1";
-                             break;
-                         default:
-                             system.win = "NT";
-                             break;
-                     }
-                 } else if (RegExp["$1"] == "9x"){
-                     system.win = "ME";
-                 } else {
-                     system.win = RegExp["$1"];
-                 }
-             }
-         }
-
-         //mobile devices
-         system.iphone = ua.indexOf("iPhone") > -1;
-         system.ipod = ua.indexOf("iPod") > -1;
-         system.nokiaN = ua.indexOf("NokiaN") > -1;
-         system.winMobile = (system.win == "CE");
-         system.macMobile = (system.iphone || system.ipod);
-
-         //gaming systems
-         system.wii = ua.indexOf("Wii") > -1;
-         system.ps = /playstation/i.test(ua);
-
-         //return it
-         return {
-             engine:     engine,
-             browser:    browser,
-             system:     system
-         };
     };
 
     /**
@@ -467,11 +296,6 @@ sec.web.renderer.utilities.JavaRendererUtilities = {};
                         value = arrKeyValue[1];
 
                         temp = decodeURIComponent(value);
-                        
-                        if(be === "Trident")
-                            temp = temp.replace(/ /g, tSpace);
-                        else if(be === "Gecko")
-                            temp = temp.replace(/ /g, gSpace);
                         
                         modifiers[key.toUpperCase()] = temp;
                     }
