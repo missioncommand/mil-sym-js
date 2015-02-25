c229b6674d9de9853547cc55a4b2e586d791d58a
Michael Spinelli <michael.spinelli@us.army.mil>
2015-02-25 10:46:04 -0500

    Fix for mapping colors.  This bug would prevent symbols with static internal colors (like sea mines in 2525C) from being the correct color.

--- web/js/src/armyc2/c2sd/renderer/utilities/UnitFontLookup.js
+++ web/js/src/armyc2/c2sd/renderer/utilities/UnitFontLookup.js
@@ -87,9 +87,16 @@ armyc2.c2sd.renderer.utilities.UnitFontLookup = (function () {
                         data.mapping1F = symbol["MAPPING1F"] || null; //MAPPING1F
                         data.mapping1N = symbol["MAPPING1N"] || null; //MAPPING1N
                         data.mapping1H = symbol["MAPPING1H"] || null; //MAPPING1H
-                        data.mapping1color = symbol["MAPPING1COLOR"] || ""; //MAPPING1COLOR
                         data.mapping2 = symbol["MAPPING2"] || null; //MAPPING2
-                        data.mapping2color = symbol["MAPPING2COLOR"] || ""; //MAPPING2COLOR
+                        
+                        if(symbol["MAPPING1COLOR"])
+                            data.mapping1color = "#" + symbol["MAPPING1COLOR"];
+                        else
+                            data.mapping1color = "";
+                        if(symbol["MAPPING2COLOR"])
+                            data.mapping2color = "#" + symbol["MAPPING2COLOR"];
+                        else
+                            data.mapping2color = "";
 
                     } 
                     if((symbolMapB[data.symbolID])===undefined)
@@ -123,9 +130,16 @@ armyc2.c2sd.renderer.utilities.UnitFontLookup = (function () {
                         data.mapping1F = symbol["MAPPING1F"] || null; //MAPPING1F
                         data.mapping1N = symbol["MAPPING1N"] || null; //MAPPING1N
                         data.mapping1H = symbol["MAPPING1H"] || null; //MAPPING1H
-                        data.mapping1color = symbol["MAPPING1COLOR"] || ""; //MAPPING1COLOR
                         data.mapping2 = symbol["MAPPING2"] || null; //MAPPING2
-                        data.mapping2color = symbol["MAPPING2COLOR"] || ""; //MAPPING2COLOR
+
+                        if(symbol["MAPPING1COLOR"])
+                            data.mapping1color = "#" + symbol["MAPPING1COLOR"];
+                        else
+                            data.mapping1color = "";
+                        if(symbol["MAPPING2COLOR"])
+                            data.mapping2color = "#" + symbol["MAPPING2COLOR"];
+                        else
+                            data.mapping2color = "";
 
                     } 
                     if((symbolMapC[data.symbolID])===undefined)
