de9b6ebe326147e9471763e3b1cf95bcb44d8805
Michael Spinelli <michael.spinelli@us.army.mil>
2015-07-02 11:22:16 -0400

    -updated version number

--- web/js/src/armyc2/c2sd/renderer/utilities/RendererSettings.js
+++ web/js/src/armyc2/c2sd/renderer/utilities/RendererSettings.js
@@ -6,7 +6,7 @@ armyc2.c2sd.renderer.utilities = armyc2.c2sd.renderer.utilities || {};
 /** @class */
 armyc2.c2sd.renderer.utilities.RendererSettings = (function () {
 	
-    var _Version = "0.0.77";
+    var _Version = "0.0.78";
 //outline approach.  none, filled rectangle, outline (default),
     //outline quick (outline will not exceed 1 pixels).
     var _SymbologyStandard = 0,
